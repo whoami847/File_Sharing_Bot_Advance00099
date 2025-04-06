4f558ca8514a9ce55bc2b2959375dd38060e12fa
whoami847 <mrbeastbangla069@gmail.com>
2025-04-06 17:08:46 +0600

    Create file_utils.py

---
+++ utils/file_utils.py
@@ -0,0 +1,11 @@
+from pyrogram import Client
+from config import DATABASE_URL
+import os
+
+def save_file(file, user_id):
+    # Logic to save files into the database
+    pass
+
+def download_file(file_id):
+    # Logic to download the file
+    pass
