e9c3c7e183f95d5f2e41727b5c7911a96c35755d
whoami847 <mrbeastbangla069@gmail.com>
2025-04-06 22:04:35 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,7 @@
-import os
-from pyrogram import Client, filters
-from fancy_font import to_fancy  # Importing the fancy font function
-from config import BOT_TOKEN  # Import the bot token from config
-from handlers import user_handler, admin_handler, media_handler
+Быстрый, и стабильный VPN прямо в Telegram ❤️
 
-# Initialize the bot with the provided token
-app = Client("file_sharing_bot", bot_token=BOT_TOKEN, api_id=os.getenv("API_ID"), api_hash=os.getenv("API_HASH"))
+🔥@SpeeeedVPNbot
 
-# Example of using fancy font in a command
-@app.on_message(filters.command('start'))
-async def start(client, message):
-    await message.reply(to_fancy("Welcome to the File Sharing Bot!"))
+Попробуйте 7 дней абсолютно бесплатно, просто нажмите в боте "/start -> начать пробный период".
 
-# Registering user, admin, and media handlers
-app.add_handler(user_handler)
-app.add_handler(admin_handler)
-app.add_handler(media_handler)
-
-# Run the bot
-app.run()
+Поддерживаются все устройства IOS/Android/Windows/Mac, роутеры, и тд!
